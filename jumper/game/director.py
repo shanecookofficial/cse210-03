a067d33c637a86f2d6770c818e032866d634e5de
shanecookofficial <shanecookofficial@gmail.com>
2022-05-19 11:30:58 -0400

    Added Director class framework
    Added guess and lives attributes in the initializer method

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,1 +1,17 @@
-# game director
+
+
+class Director():
+
+    def __init__(self) -> None:
+        self.guess = ""
+        self.lives = 5
+        
+
+    def start_game():
+        pass
+
+    def get_inputs():
+        pass
+
+    def do_outputs():
+        pass
